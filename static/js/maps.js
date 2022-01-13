1d833510e329677fee4c6f1c0ffdcf19414d0b45
Ehnvik <gustav.ehnvik@gmail.com>
2022-01-13 21:12:01 +0000

    Add google maps api and finish contact page

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,1 +1,24 @@
-console.log("Hello")
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 14,
+        center: {
+            lat: 54.97474439256173,
+            lng: -1.619282792601715
+        }
+    });
+
+    var labels = "A";
+
+    var locations = [
+        { lat: 54.97474439256173, lng: -1.619282792601715 }
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
